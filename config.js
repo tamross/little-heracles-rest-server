28783a11ee5fdfb2724fb0557534e7cf2ef9a22e
Tamara Ross <tamross@gmail.com>
2016-09-28 09:37:08 +1000

    can deploy to bluemix

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/littleheracles',
+    //'mongoUrl' : 'mongodb://localhost:27017/littleheracles',
+    'mongoUrl' : 'mongodb://admin:adminadmin@ds021326.mlab.com:21326/littleheracles'
     // Prod mongo url
     // mongodb://admin:adminadmin@ds021326.mlab.com:21326/littleheracles
     // 'facebook': {
