8ceebfdfc2650731f4d7d6dcafb4596acc485547
Tamara Ross <tamross@gmail.com>
2016-10-02 20:34:05 +1100

    added UI

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/littleheracles'
-    //'mongoUrl' : 'mongodb://admin:adminadmin@ds021326.mlab.com:21326/littleheracles'
+    //'mongoUrl' : 'mongodb://localhost:27017/littleheracles'
+    'mongoUrl' : 'mongodb://admin:adminadmin@ds021326.mlab.com:21326/littleheracles'
 
 }
